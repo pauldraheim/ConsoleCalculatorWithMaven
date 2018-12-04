3043f94fe5f5bd457b8117d81fb96b2d1259caa7
pauldraheim <paul.draheim@gmx.ch>
2018-12-04 12:16:51 +0100

    Added Calculator class containing summe method to be able execute Addition

---
+++ Workspace/02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/draheim/consolecalculator/Calculator.java
@@ -0,0 +1,14 @@
+package ch.bbw.draheim.consolecalculator;
+
+/**
+ * Calculator
+ *   - kann zwei Zahlen addieren
+ *   
+ * @author Paul Draheim
+ * @version 0.0.1
+ */
+public class Calculator {
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+}
